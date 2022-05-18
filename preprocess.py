380aebbba97e7a59a14d6b129545c1477a4059b3
Nicolas Perrin <perrin.isir@gmail.com>
2022-05-18 19:10:09 +0200

    testing nbconvert preprocess
    Former-commit-id: 9351376ac8c9a41d5e32e7f72c7cc46d32aab3b5

--- preprocess.py
+++ preprocess.py
@@ -9,19 +9,19 @@ class CustomClearOutputPreprocessor(ClearOutputPreprocessor):
 
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
-        self.skip = True
 
     def preprocess_cell(self, cell, resources, cell_index):
         """
         Apply a transformation on each cell. See base.py for details.
         """
         if cell.cell_type == "code":
-            if not self.skip or cell.source != "":
+            if cell.source is None or cell.source == "":
+                print("ok")
+            else:
                 cell.outputs = []
                 cell.execution_count = None
                 # Remove metadata associated with output
                 if "metadata" in cell:
                     for field in self.remove_metadata_fields:
                         cell.metadata.pop(field, None)
-            self.skip = False
         return cell, resources
